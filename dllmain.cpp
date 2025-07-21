4c9cbc966a1a16cd91b7e789ad9f6509d6d6787f
SadnessPower <robertolamaestra@gmail.com>
2025-07-21 02:09:27 +0200

    Disable only sound

--- dllmain.cpp
+++ dllmain.cpp
@@ -62,78 +62,6 @@ typedef h3::H3CombatMonster _BattleStack_;
 */
 
 // ----------------------------------------------
-/*
-_LHF_(AIPickResourcesNoPause1_004A45E2)
-{
-	bool isHumanPlayer = *(bool*)(c->ebp + 0x14);
-	BOOL8 goSolo = *(BOOL8*)0x00691259;
-
-	if (!isHumanPlayer || goSolo)
-	{
-		c->return_address = 0x004A467A;
-		return SKIP_DEFAULT;
-	}
-
-	return EXEC_DEFAULT;
-}
-
-_LHF_(AIPickResourcesNoPause2_004A479A)
-{
-	bool isHumanPlayer = *(bool*)(c->ebp + 0x14);
-	BOOL8 goSolo = *(BOOL8*)0x00691259;
-
-	if (!isHumanPlayer || goSolo)
-	{
-		c->return_address = 0x004A4832;
-		return SKIP_DEFAULT;
-	}
-
-	return EXEC_DEFAULT;
-}
-
-_LHF_(AIPickResourcesNoPause3_004A4978)
-{
-	bool isHumanPlayer = *(bool*)(c->ebp + 0x14);
-	BOOL8 goSolo = *(BOOL8*)0x00691259;
-
-	if (!isHumanPlayer || goSolo)
-	{
-		c->return_address = 0x004A4A10;
-		return SKIP_DEFAULT;
-	}
-
-	return EXEC_DEFAULT;
-}
-
-_LHF_(AIPickTreasureChestNoPause_004A62C3)
-{
-	bool isHumanPlayer = *(bool*)(c->ebp + 0x14);
-	BOOL8 goSolo = *(BOOL8*)0x00691259;
-
-	if (!isHumanPlayer || goSolo)
-	{
-		c->return_address = 0x004A635B;
-		return SKIP_DEFAULT;
-	}
-
-	return EXEC_DEFAULT;
-}
-
-_LHF_(AIPickCampfireNoPause_004A0E52)
-{
-	bool isHumanPlayer = *(bool*)(c->ebp + 0x14);
-	BOOL8 goSolo = *(BOOL8*)0x00691259;
-
-	if (!isHumanPlayer || goSolo)
-	{
-		c->return_address = 0x004A0EEA;
-		return SKIP_DEFAULT;
-	}
-
-	return EXEC_DEFAULT;
-}
-*/
-
 _LHF_(DisableAIAllEventsDelay_004A818F)
 {
 	bool isHumanPlayer = *(bool*)(c->ebp + 0x14);
@@ -220,7 +148,6 @@ _LHF_(DisableAICombatDelay7_004A6B88)
 }
 
 
-
 extern "C" __declspec(dllexport) BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
@@ -235,22 +162,44 @@ extern "C" __declspec(dllexport) BOOL APIENTRY DllMain(HMODULE hModule,
 			Z_DisableAITurnDelay = globalPatcher->CreateInstance(PINSTANCE_MAIN);
 			Era::ConnectEra(hModule, PINSTANCE_MAIN);
 
-			Z_DisableAITurnDelay->WriteLoHook(0x004A818F, DisableAIAllEventsDelay_004A818F);
+/*			Z_DisableAITurnDelay->WriteLoHook(0x004A818F, DisableAIAllEventsDelay_004A818F);
 			Z_DisableAITurnDelay->WriteLoHook(0x004ADF40, DisableAICombatDelay1_004ADF40);
 			Z_DisableAITurnDelay->WriteLoHook(0x004AE5D1, DisableAICombatDelay2_004AE5D1);
 			Z_DisableAITurnDelay->WriteLoHook(0x004AE5C2, DisableAICombatDelay3_004AE5C2);
 			Z_DisableAITurnDelay->WriteLoHook(0x004AE1FE, DisableAICombatDelay4_004AE1FE);
 			Z_DisableAITurnDelay->WriteLoHook(0x004A6996, DisableAICombatDelay5_004A6996);
 			Z_DisableAITurnDelay->WriteLoHook(0x004A75BC, DisableAICombatDelay6_004A75BC);
-			Z_DisableAITurnDelay->WriteLoHook(0x004A6B88, DisableAICombatDelay7_004A6B88);			
+			Z_DisableAITurnDelay->WriteLoHook(0x004A6B88, DisableAICombatDelay7_004A6B88);			*/
 			
-			/*
-			Z_DisableAITurnDelay->WriteLoHook(0x004A479A, AIPickResourcesNoPause1_004A45E2);
-			Z_DisableAITurnDelay->WriteLoHook(0x004A479A, AIPickResourcesNoPause2_004A479A);
-			Z_DisableAITurnDelay->WriteLoHook(0x004A4978, AIPickResourcesNoPause3_004A4978);
-			Z_DisableAITurnDelay->WriteLoHook(0x004A62C3, AIPickTreasureChestNoPause_004A62C3);
-			Z_DisableAITurnDelay->WriteLoHook(0x004A0E52, AIPickCampfireNoPause_004A0E52);
-			*/
+			// Remove fading sound delay everywhere
+			Z_DisableAITurnDelay->WriteHexPatch(0x0059B4FF, "EB");
+			
+			// Remove delay while playing sound
+			Z_DisableAITurnDelay->WriteHexPatch(0x0049DEA3, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x0049E622, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x0049E930, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x0049EBEC, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x0049F3B8, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A0ABE, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A0E89, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A2052, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A3DDA, "E9 5A 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A4619, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A47D1, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A49AF, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A4C06, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A4E7D, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A505D, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A5A8B, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A5C90, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A62FA, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A69DC, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A6BCE, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A6E3C, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A71B0, "E9 5D 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004A9877, "E9 5C 00 00 00");
+			Z_DisableAITurnDelay->WriteHexPatch(0x004AC91E, "E9 5C 00 00 00"); // FizzleCenter function
+
 			break;
 		}
 		case DLL_THREAD_ATTACH:
